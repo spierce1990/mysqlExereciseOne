5230a446ed7880fdf168e80ce24e19da1d17f9fd
Sionylen <sionylenpierce@outlook.com>
2022-12-31 17:35:52 -0700

    added

--- exercise.sql
+++ exercise.sql
@@ -10,10 +10,12 @@ FROM products
 WHERE price = 1400;
 
 -- find all Macbooks
--- select * from customer where row_number() = 3
 SELECT*
 FROM products
-WHERE Name = Macbook;
+WHERE Name = 'Macbook';
+-- SELECT*
+-- FROM products
+-- WHERE Name = Macbook;
 
 -- find all products that cost $11.99 or $13.99
 
@@ -26,7 +28,7 @@ WHERE price = 11.99  or 13.99;
 
 SELECT * 
 FROM products
-WHERE price = 11.99 = 0;
+WHERE price  = 11.99 = 0;
 
 -- find all products and sort them by price from greatest to least
 
@@ -42,7 +44,7 @@ FROM employees
 WHERE MiddleInitial IS null;
 
 -- find distinct product prices
- SELECT DISTINCT price
+ SELECT DISTINCT price, name
 FROM products;
 
 -- find all employees whose first name starts with the letter ‘j’
@@ -60,7 +62,10 @@ SELECT AVG(price)
 FROM products;
 
 -- find all Geek Squad employees who don't have a middle initial
-
+SELECT*
+FROM employees
+WHERE MiddleInitial = null
+AND Title Like '%Geeksquad%';
 -- SELECT* 
 -- FROM employees
 
@@ -68,6 +73,6 @@ FROM products;
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
 SELECT* 
 FROM products
-WHERE StockLevel between 500 AND 1200 order by StockLevel desc;
+WHERE StockLevel between 500 AND 1200 order by price asc;
 
 
